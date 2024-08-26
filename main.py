84f042fcfa79077696d5aea219971a8c0ecee213
Savic Georgije 43690219 <georgije.savic@ubs.com>
2024-08-26 11:49:39 +0200

    pp

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-# Dein Code kommt hier hin
+"""
+Aufgabe:
+"""
+import json
+
+
+def load_recipe(json_string):
+    return json.loads(json_string)
+
+
+def adjust_recipe(recipe_, num_people):
+    factor = num_people / recipe_['servings']
+    adjusted_ingredients = {ingredient: amount * factor for ingredient, amount in recipe_['ingredients'].items()}
+    adjusted_recipe_ = recipe_.copy()
+    adjusted_recipe_['ingredients'] = adjusted_ingredients
+    adjusted_recipe_['servings'] = num_people
+    return adjusted_recipe_
+
 
 if __name__ == '__main__':
-    # Beispiel für die Datenstruktur eines Rezepts
     recipe_json = '{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}'
-    # Dein Code kommt hier hin
+    recipe = load_recipe(recipe_json)
+    adjusted_recipe = adjust_recipe(recipe, 2)
+    print(json.dumps(adjusted_recipe, indent=4))
